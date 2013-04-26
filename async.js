b621a6ffd7afbe02818bd1be1688a6143f5433f8
Jon Perl <perl.jonathan@gmail.com>
2013-04-25 23:17:51 -0400

    update to latest package api

---
+++ async.js
@@ -0,0 +1,1 @@
+async = Npm.require("async");
--- package.js
+++ package.js
@@ -1,12 +1,11 @@
 Package.describe({
-  summary: "Meteor smart package for async node.js package"
+    summary: "Meteor smart package for async node.js package"
+});
+
+Npm.depends({
+    "async": "0.2.7"
 });
 
 Package.on_use(function (api) {
-  api.use('coffeescript', 'server');
-
-  api.add_files([
-    'bootstrap.coffee',
-    'server.coffee'
-  ], 'server');
+    api.add_files("async.js", ["client", "server"]);
 });
