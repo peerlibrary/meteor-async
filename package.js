091ee551eafc86aab25b4edc642b6a59750eae6f
Mitar <mitar.git@tnode.com>
2013-06-27 03:39:06 -0700

    Added tests.

--- package.js
+++ package.js
@@ -2,12 +2,13 @@ Package.describe({
   summary: "Meteor smart package for async node.js package"
 });
 
-Npm.depends({
-  async: "0.2.9"
-});
-
 Package.on_use(function (api) {
   api.add_files([
-    'async.js'
+    'async/lib/async.js'
   ], ['client', 'server']);
+});
+
+Package.on_test(function (api) {
+  api.use(['async', 'tinytest', 'test-helpers'], ['client', 'server']);
+  api.add_files('tests.js', ['client', 'server']);
 });
---
+++ tests.js
@@ -0,0 +1,18 @@
+Tinytest.addAsync('meteor-async', function (test, onComplete) {
+  var isDefined = false;
+  try {
+    async;
+    isDefined = true;
+  }
+  catch (e) {
+  }
+  test.isTrue(isDefined, "async is not defined");
+
+  async.each(['a'], function (item, cb) {
+    test.equal(item, 'a', "Iterated item not equal");
+    cb();
+  }, function (err) {
+    test.isFalse(err, "Error iterating: " + err);
+    onComplete();
+  });
+});
