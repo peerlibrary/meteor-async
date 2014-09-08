86b80568778380b42369a1606d6397c45086adec
Mitar <mitar.git@tnode.com>
2014-09-07 21:07:59 -0700

    Transitory package.

--- package.js
+++ package.js
@@ -1,21 +1,10 @@
 Package.describe({
   summary: "Async provides straight-forward, powerful functions for working with asynchronous JavaScript",
   version: '0.9.0-1',
-  name: 'peerlibrary:async',
+  name: 'mrt:async',
   git: 'https://github.com/peerlibrary/meteor-async.git'
 });
 
 Package.on_use(function (api) {
-  api.versionsFrom("METEOR@0.9.1");
-  api.export('async');
-
-  api.add_files([
-    'async/lib/async.js',
-    'export-async.js'
-  ], ['client', 'server']);
-});
-
-Package.on_test(function (api) {
-  api.use(["peerlibrary:async", 'tinytest', 'test-helpers'], ['client', 'server']);
-  api.add_files('tests.js', ['client', 'server']);
+  api.imply('peerlibrary:async');
 });
