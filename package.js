bccf79fc7dc5e4e33d96ab17026ee1162b607455
Mitar <mitar.git@tnode.com>
2014-09-08 12:50:17 -0700

    Version bump.

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
   summary: "Async provides straight-forward, powerful functions for working with asynchronous JavaScript",
-  version: '0.9.0-1',
+  version: '0.9.0-2',
   name: 'mrt:async',
   git: 'https://github.com/peerlibrary/meteor-async.git'
 });
 
 Package.on_use(function (api) {
-  api.imply('peerlibrary:async');
+  api.imply('peerlibrary:async@0.9.0-2');
 });
