3c7e3e0b0a4e8eaa04676d3e77112a398889e054
Mitar <mitar.git@tnode.com>
2015-01-16 20:02:58 -0800

    Updated version.

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
   summary: "Async provides straight-forward, powerful functions for working with asynchronous JavaScript",
-  version: '0.9.0_3',
+  version: '0.9.2_1',
   name: 'peerlibrary:async',
   git: 'https://github.com/peerlibrary/meteor-async.git'
 });
 
 Package.on_use(function (api) {
-  api.versionsFrom("METEOR@0.9.3");
+  api.versionsFrom('METEOR@1.0.2.1');
   api.export('async');
 
   api.add_files([
